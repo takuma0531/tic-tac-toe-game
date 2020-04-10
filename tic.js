dc22d14bbd6d2dd45714b53817a2728364e431ce
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-10 12:55:28 +0300

    add changeTurn() and display 'o' or 'x'

--- tic.js
+++ tic.js
@@ -2,12 +2,25 @@
 
 let currentPlayer = 'A';
 
+
+changeTurn = () => {
+  currentPlayer = currentPlayer === 'A' ? 'B' : 'A'
+}
+
+const clickCell = (e) => {
+
+  const clickedCell = e.target;
+
+  if (currentPlayer === 'A') {
+    clickedCell.innerHTML = 'o';
+  } else {
+    clickedCell.innerHTML = 'x';
+  }
+
+  changeTurn();
+}
+
 const cells = document.querySelectorAll('.cell');
 cells.forEach(cell => {
   cell.addEventListener('click', clickCell, {once: true})
 });
-
-
-function clickCell() {
-
-}
