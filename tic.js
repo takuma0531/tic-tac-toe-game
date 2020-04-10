b7780d829d243de4fcba685dc6e2c2918903070f
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-10 13:16:58 +0300

    add a function which manages game condition

--- tic.js
+++ tic.js
@@ -1,21 +1,37 @@
 // game state
-
+let cellState = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'A';
+let isFinished = false;
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 
 changeTurn = () => {
   currentPlayer = currentPlayer === 'A' ? 'B' : 'A'
 }
 
+StateGameCondition = (clickedCell, clickedCellIndex)  => {
+  cellState[clickedCellIndex] = currentPlayer;
+
+  currentPlayer === 'A' ? clickedCell.innerHTML = 'o' : clickedCell.innerHTML = 'x';
+  console.log(cellState)
+}
+
 const clickCell = (e) => {
 
   const clickedCell = e.target;
+  const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-  if (currentPlayer === 'A') {
-    clickedCell.innerHTML = 'o';
-  } else {
-    clickedCell.innerHTML = 'x';
-  }
+  StateGameCondition(clickedCell, clickedCellIndex)
 
   changeTurn();
 }
